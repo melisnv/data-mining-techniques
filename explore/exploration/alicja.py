db3830a7cf8ad38ec73ced005c8ff6425febb8fb
AlicjaDobrzeniecka <alizjonizm@gmail.com>
2022-04-08 19:04:50 +0200

    Starts exploration

---
+++ explore/exploration/alicja.py
@@ -0,0 +1,23 @@
+"""Once you have chosen (and perhaps installed) the software, here is your first task:
+
+• Download the ODI dataset from BB. ODI stands for Own Dataset Initiative.
+
+• Load the dataset, which will be in CSV format as well as Excel, into your software. CSV
+stands for comma separated values, though it is more common these days to separate
+values with semi-colon (;). Nevertheless, the format is still called CSV even if the sepa-
+rator is a tab character or whatever else defined by the dataset creator.
+
+• Notice all sorts of properties of the dataset: how many records are there, how many
+attributes, what kinds of attributes are there, ranges of values, distribution of values,
+relationships between attributes, and so on. Notice if something is interesting (to you,
+or in general), make sure you write it down if you find something worth mentioning.
+
+• Make various plots of the data. Is there something interesting worth reporting? Re-
+port the figures, discuss what is in them. What meaning do those bars, lines, dots, etc.
+convey? Please select essential and interesting plots for discussion, as you have limited
+space for reporting your findings (see details in a later section).
+To sum up, you will need to explore the dataset, and report findings that are essential to get
+an idea about the data, and also, findings that make it possible to learn something interesting
+about the dataset.
+"""
+
